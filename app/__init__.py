d5eb676a766108b4f0e3809c823b2ddb9f6a8f70
nadxelle <nadxellehernandez@gmail.com>
2023-02-22 17:35:26 -0800

    everything back to what it was

--- app/__init__.py
+++ app/__init__.py
@@ -15,13 +15,8 @@ def create_app(test_config=None):
     app = Flask(__name__)
     app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
     app.config["JWT_SECRET_KEY"] = os.environ.get("JWT_SECRET_KEY")
-    uri = os.environ.get("SQLALCHEMY_DATABASE_URI")
-    # print(uri)
-    # if uri.startswith("postgres://"):
-    #     uri = uri.replace("postgres://", "postgresql+psycopg2://",1)
-    #     print(uri)
     if test_config is None:
-        app.config["SQLALCHEMY_DATABASE_URI"] = uri
+        app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get("SQLALCHEMY_DATABASE_URI")
     else:
         app.config["TESTING"] = True
         app.config["SQLALCHEMY_DATABASE_URI"] = os.environ.get(
